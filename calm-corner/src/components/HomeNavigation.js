e4a9b4766868c035617e01c40542322ff18a2059
marsdev-v <moshe.marciano@mail.huji.ac.il>
2023-10-15 17:19:44 +0300

    fixes

--- calm-corner/src/components/HomeNavigation.js
+++ calm-corner/src/components/HomeNavigation.js
@@ -10,11 +10,11 @@ import supportIcon from "../assets/images/support-icon.png"
 
 function HomeNavigation(props) {
     return (<div className="navigation-container">
-        <NavigationCard  onClick={() => alert("Hello from here")} image={firstAidIcon} text={"עזרה ראשונה"}></NavigationCard>
-        <NavigationCard image={signalIcon} text={"דיווחים מהשטח"}></NavigationCard>
-        <NavigationCard image={storiesIcon} text={"סיפורים אישיים"}></NavigationCard>
-        <NavigationCard image={methodsIcon} text={"שיטות הרגעה"}></NavigationCard>
-        <NavigationCard image={supportIcon} text={"מוקדי תמיכה"}></NavigationCard>
+        <NavigationCard image={firstAidIcon} title={"עזרה ראשונה"}></NavigationCard>
+        <NavigationCard image={signalIcon} title={"דיווחים מהשטח"}></NavigationCard>
+        <NavigationCard image={storiesIcon} title={"סיפורים אישיים"}></NavigationCard>
+        <NavigationCard image={methodsIcon} title={"שיטות הרגעה"}></NavigationCard>
+        <NavigationCard image={supportIcon} title={"מוקדי תמיכה"}></NavigationCard>
         </div>
     );
 
--- calm-corner/src/components/NavigationCard.js
+++ calm-corner/src/components/NavigationCard.js
@@ -4,10 +4,10 @@ import "../assets/styles/NavigationCard.css"
 
 function NavigationCard(props) {
     return (
-        <div onClick={() => alert("Hello from here")} className="navigation-card">
+        <div className="navigation-card">
             <a href={props.link}>
                 <img className="navigation-card-icon" src={props.image} alt={props.alt} />
-                <p className="navigation-card-text">{props.text}</p>
+                <p className="navigation-card-text">{props.title}</p>
             </a>
         </div>
     );

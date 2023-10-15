23780743aa8f2c1adece82fb1ea12ed700fadf6e
or1324 <59084841+or1324@users.noreply.github.com>
2023-10-15 15:39:20 +0300

    Update Section.js

--- calm-corner/src/react-components/Section.js
+++ calm-corner/src/react-components/Section.js
@@ -5,14 +5,10 @@ function Section(props) {
     <div>
       {props.children}
       <div class="section_header">
-        <h2>כותרת</h2>
+        <h2>{props.title}</h2>
       </div>
       <div class="section_content">
-        <p>
-          לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית הועניב היושבב שערש
-          שמחויט - שלושע ותלברו חשלו שעותלשך וחאית נובש ערששף. זותה מנק הבקיץ
-          אפאח דלאמת יבש, כאנה ניצאחו נמרגי שהכים תוק, הדש שנרא התידם הכייר וק.
-        </p>
+          {props.content}
       </div>
     </div>
   );

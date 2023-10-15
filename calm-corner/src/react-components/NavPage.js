c7fd0e26f8306c4aa86b4df4dc51ba308e711eba
Noya Cohen <noyacohen321@gmail.com>
2023-10-16 00:39:51 +0300

    fixing previous to match the design more

--- calm-corner/src/react-components/NavPage.js
+++ calm-corner/src/react-components/NavPage.js
@@ -4,7 +4,15 @@ import NavigationBar from "./NavigationBar";
 import "../assets/styles/NavPage.css";
 
 function NavPage(props) {
-    return (<Page><NavigationBar /><div id="content"><span id="title">{props.title}</span><br /><br /><span id="explaination">{props.explaination}</span><br /><br /><div id="mainContent">{props.children}</div></div></Page>);
+    return (<Page>
+        <NavigationBar />
+        <div id="content">
+            <span id="title">{props.title}</span>
+            <br /><br />
+            <span id="explaination">{props.explaination}</span>
+            <br /><br />
+            <div id="mainContent">{props.children}</div></div>
+    </Page>);
 }
 
 export default NavPage;

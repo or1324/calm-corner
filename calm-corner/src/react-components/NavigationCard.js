ccb0d7bc10932cd3e4c63b82c97478bd70892677
Noya Cohen <noyacohen321@gmail.com>
2023-10-16 23:50:51 +0300

    almost finished mobile design home page

--- calm-corner/src/react-components/NavigationCard.js
+++ calm-corner/src/react-components/NavigationCard.js
@@ -5,10 +5,10 @@ import "../assets/styles/NavigationCard.css"
 function NavigationCard(props) {
     return (
         <div className="navigation-card">
-            <a href={props.link}>
+            {/* <a href={props.link}> */}
                 <img className="navigation-card-icon" src={props.image} alt={props.alt} />
                 <p className="navigation-card-text">{props.title}</p>
-            </a>
+            {/* </a> */}
         </div>
     );
 

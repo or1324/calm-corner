ff6b80df4589d06ff7189466a9de95625d23bf38
Noya Cohen <noyacohen321@gmail.com>
2023-10-16 20:23:15 +0300

    some more changes

--- calm-corner/src/react-components/Page.js
+++ calm-corner/src/react-components/Page.js
@@ -1,14 +1,16 @@
 import React from "react";
-import "../assets/styles/Page.css";
+import "../assets/styles/Page.scss";
 import Footer from "./Footer";
 function Page(props) {
   // Add the content of your page here
   return (
     <div id="body">
-      {props.children}
-      <Footer/>
+      <div className="main-container">
+        {props.children}
+      </div>
+      <Footer />
     </div>
-    );
+  );
 }
 
 export default Page;
